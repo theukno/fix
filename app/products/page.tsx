60e7c2fa70ef38c75c2753a7c0c199618c722535
Joslyn <89780037+theukno@users.noreply.github.com>
2025-04-01 19:25:54 +0530

    Update page.tsx

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { createContext, useContext, useState } from 'react';
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 import { Button } from "@/components/ui/button";
